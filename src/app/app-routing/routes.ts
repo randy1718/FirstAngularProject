b4f645bbe09c393f23bfc431e72d6397119ca0c9
Randy Rojas <rrandymiller@gmail.com>
2023-06-25 23:09:48 -0500

    commit 25 june

--- src/app/app-routing/routes.ts
+++ src/app/app-routing/routes.ts
@@ -1,14 +1,16 @@
 import { Routes } from '@angular/router';
 
-import { MenuComponent } from '../menu/menu.component';
+
 import { DishdetailComponent } from '../dishdetail/dishdetail.component';
 import { HomeComponent } from '../home/home.component';
 import { AboutComponent } from '../about/about.component';
+import { MenuComponent } from '../menu/menu.component';
 import { ContactComponent } from '../contact/contact.component';
 
 export const routes: Routes = [
   { path: 'contactus', component: ContactComponent },
   { path: 'home', component: HomeComponent },
   { path: 'menu', component: MenuComponent },
+  { path: 'dishdetail/:id',     component: DishdetailComponent },
   { path: '', redirectTo: '/home', pathMatch: 'full' }
 ];
--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, Input} from '@angular/core';
-import {Dish} from '../shared/dish';
-
+import { Dish } from '../shared/dish';
+import { DishService } from '../services/dish.service';
+import { Params, ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-dishdetail',
@@ -10,11 +12,18 @@ import {Dish} from '../shared/dish';
 
 export class DishdetailComponent implements OnInit{
 
-  @Input()
-  dish: Dish | undefined; 
+  dish!: Dish;
 
-   
-  ngOnInit(){
+  constructor(private dishservice: DishService,
+    private route: ActivatedRoute,
+    private location: Location) { }
 
+  ngOnInit() {
+    const id = this.route.snapshot.params['id'];
+    this.dish = this.dishservice.getDish(id);
+  }
+
+  goBack(): void {
+    this.location.back();
   }
 }
