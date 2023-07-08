bf806ac5f77c7ea479c6c1af1776bd0de1a08e7d
Randy Rojas <rrandymiller@gmail.com>
2023-07-08 16:24:33 -0500

    commit antes de arreglo tecnico

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -29,7 +29,11 @@ import { MatDialogModule } from '@angular/material/dialog';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatCheckboxModule } from '@angular/material/checkbox';
-import { FormsModule } from '@angular/forms'; 
+import { FormsModule } from '@angular/forms';
+  
+import { MatSelectModule } from '@angular/material/select';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
